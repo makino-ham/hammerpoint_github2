e23dbaa7fdacb612bad3c53b77622950781449d8
ebata-ham <93498062+ebata-ham@users.noreply.github.com>
2021-12-14 11:59:25 +0900

    1214江幡

---
+++ Password.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Password {
+
+		private int siskanid;
+		private String name;
+		private String pass;
+
+
+		public Password() {}
+		public Password(int siskanid,String name,String pass) {
+			this.siskanid=siskanid;
+			this.name=name;
+			this.pass=pass;
+		}
+		public Password(int siskanid, String pass) {
+			this.siskanid=siskanid;
+			this.pass=pass;
+		}
+		public Password(String pass, int siskanid) {
+			this.pass = pass;
+			this.siskanid = siskanid;
+		}
+		public int getSiskanid() {
+			return siskanid;
+		}
+		public void setSiskanid(int siskanid) {
+			this.siskanid = siskanid;
+		}
+
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getPass() {
+			return pass;
+		}
+		public void setPass(String pass) {
+			this.pass = pass;
+		}
+
+
+}
